f880ac86d94e2fce8519aa13c32dd9eef7adc697
Tuan Ha <hatuan@email.cz>
2026-05-10 02:03:47 +0200

    Inital header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,13 @@ function Header({score, bestScore, limit, onLimitChange}) {
     <header>
       <div>
         <h1>Memory Card Game</h1>
-        <p>Catch every Pokemon exactly once! Catch them again and you LOSE!</p>
+        <p className="instructions">Catch every Pokemon exactly once! Catch them again and you LOSE!</p>
       </div>
-      <div>
-        <p>Current Score: {score}</p>
-        <p>Best Score: {bestScore}</p>
+      <div className="header-sidebar sidebar">
+        <div className="scoreboard">
+          <p>Current Score: {score}</p>
+          <p>Best Score: {bestScore}</p>
+        </div>
         <select name="limit" id="limit" onChange={onLimitChange} value={limit}>
           <option value="10">Easy</option>
           <option value="15">Medium</option>
